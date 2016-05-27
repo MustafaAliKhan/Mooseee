eebb3fdef93a41dd8fc4d59c5ca0180dba2b338f
Mustafa Khan <moofakhan@gmail.com>
2016-05-26 23:53:33 -0400

    added users and log in abilities

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # make sure to set this 
+  config.action_mailer.default_url_options = { :host => 'mooseee.com' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :universities
+  get 'pages/universities'
   get 'pages/home'
   root "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
