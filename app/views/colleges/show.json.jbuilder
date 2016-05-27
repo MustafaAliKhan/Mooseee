e007b763c6fb7db06d8ae98b5787806115e1056b
Mustafa Khan <moofakhan@gmail.com>
2016-05-26 20:34:36 -0400

    deleted scaffolding (maybe)

--- app/views/colleges/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @college, :id, :name, :location, :description, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160526215637) do
 
-  create_table "colleges", force: :cascade do |t|
-    t.string   "name"
-    t.string   "location"
-    t.string   "description"
-    
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+  
 
 end
